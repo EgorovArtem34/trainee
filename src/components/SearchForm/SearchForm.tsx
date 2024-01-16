54a91d27d1d97afbf1f84bc42ca686bf674d7857
artem_egorov <artegtor@gmail.com>
2024-01-16 18:13:11 +0500

    add header: SearchForm

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,13 +1,34 @@
+import { AiOutlineSearch } from "react-icons/ai";
 import { useAppDispatch, useAppSelector } from "@/store/hook";
-import { fetchUsers } from "@/store/slices/usersSlice";
 import styles from "./SearchForm.module.scss";
+import { setSearchQuery } from "@/store/slices/usersSlice";
+import { Button } from "@/ui/Button/Button";
 
 export const SearchForm = () => {
   const dispatch = useAppDispatch();
+  const { searchQuery } = useAppSelector((state) => state.usersSlice);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
-  return <form className={styles.formSearch} onSubmit={handleSubmit}></form>;
+  return (
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+        <input
+          type="search"
+          placeholder="search users..."
+          value={searchQuery}
+          onChange={(e) => dispatch(setSearchQuery(e.target.value))}
+          className={styles.inputSearch}
+        />
+        <div className={styles.inputLoupe}>
+          <AiOutlineSearch />
+        </div>
+      </div>
+      <Button type="submit" variant="search">
+        Search
+      </Button>
+    </form>
+  );
 };
---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode, ButtonHTMLAttributes } from "react";
+import cn from "classnames";
+import styles from "./Button.module.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  variant: string;
+  type?: "button" | "submit" | "reset";
+}
+
+export const Button: React.FC<ButtonProps> = (props) => {
+  const { children, variant, type = "button", ...rest } = props;
+  const buttonClass = cn(styles.button, {
+    [styles[variant]]: variant,
+  });
+
+  return (
+    <button className={buttonClass} type={type} {...rest}>
+      {children}
+    </button>
+  );
+};
